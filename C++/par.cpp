35cc19f024050873cf306742758d7d5686be1fd7
wobushimotou4 <1687734258@qq.com>
2019-04-08 21:51:34 +0800

    Cp 15 2/3

--- C++/par.cpp
+++ C++/par.cpp
@@ -2,13 +2,87 @@
 #include <string>
 
 using namespace std;
+class Quote{
+private:
+    string book;
+protected:
+    double price;
+public:
+    Quote() = default;
+    Quote(double num,const string &name) : book(name),price(num) {}
+    string isbn() const{ return book; }
+    virtual double net_price(size_t n) const { return n*price; };
+    virtual string debug() const { return book+" "+to_string(price);}
+};
+class Bulk_quote : public Quote{
+protected:
+    size_t min_qty = 0;
+    double discount = 0.0;
+public:
+    Bulk_quote() = default;
+    Bulk_quote(double num,const string &name,size_t n,double dc) : Quote(num,name),min_qty(n),discount(dc) {  }
+    double net_price(size_t n) const override;
+    string debug() const override { return Quote::debug()+" "+to_string(min_qty)+" "+to_string(discount); }
+};
+class Limit_quote : public Bulk_quote{
+public:
+    Limit_quote() = default;
+    Limit_quote(double num,const string &name,size_t n,double dc) : Bulk_quote(num,name,n,dc){}
+    double net_price(size_t n) const override;
+    string debug() const override { return Bulk_quote::debug();}
+
+};
+
+double Bulk_quote::net_price(size_t n) const
+{
+    if(n >= min_qty)
+        return n*(1-discount)*price;
+    else
+        return n*price;
+}
+
+double Limit_quote::net_price(size_t n) const
+{
+    if(n <= min_qty)
+        return n*(1-discount)*price;
+    else
+        return n*price;
+}
+double print_total(ostream &os,const Quote &item,size_t n)
+{
+    double ret = item.net_price(n);
+    os << "ISBN: " << item.isbn() << " #sold: " << n << " total due: " << ret << endl;
+    return ret;
+}
+
+class Disc_quote : public Quote{
+protected:
+    size_t quantity = 0;
+    double discount = 0.0;
+public:
+    Disc_quote() = default;
+    Disc_quote(const string &book,double price,size_t qty,double dc) : Quote(price,book),quantity(qty),discount(dc) {  }
+    double net_price(size_t n)const = 0;
+};
+
+class Bulk_quote2 : public Disc_quote{
+public:
+    Bulk_quote2() = default;
+    Bulk_quote2(const string &book,double price,size_t qty,double dc) : Disc_quote(book,price,qty,dc) {  }
+    double net_price(size_t n)const override;
+};
+double Bulk_quote2::net_price(size_t n)const
+{
+    if(n >= quantity)
+        return n*(1-discount)*price;
+    else
+        return n*price;
+}
 int main()
 {
-    std::string s1 = "wangheng";
-    auto p = s1.back();
-    cout << p << endl;
-    
-
+    Bulk_quote2 bq2("wh",10,10,0.2);
+    cout << bq2.Quote::net_price(10) << endl;
+    cout << bq2.net_price(10) << endl;
     return 0;
 }
 
--- project/server.cpp
+++ project/server.cpp
@@ -2,40 +2,117 @@
 #include <iostream>
 #include <istream>
 #include <string>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <thread>
  
 using namespace std;
- 
-int main()
+int Send(int fd,const char *str,int size)
 {
+    int l = 0;
+    if((l += send(fd,&size,sizeof(size),0)) > 0)
+        send(fd,str,size,0);
+    return l;
+
+}
+void handle(int fd)
+{    
     redisContext* c = redisConnect("127.0.0.1", 6379);
     if(c->err)
     {
         redisFree(c);
         cout << "Connect to redisServer fail" << endl;
-        return 1;
+        return;
     }
 
     string command;
     redisReply *r;
-    cout << "127.0.0.1:6379 > ";
-    while(r->type != REDIS_REPLY_ERROR) {
-        getline(cin,command);
-        r = (redisReply*)redisCommand(c, command.c_str());
-            
-        if(r->type == REDIS_REPLY_STRING)   //返回值为字符串
-            cout << r->str << endl;
-        else if(r->type == REDIS_REPLY_INTEGER) //返回值为整数
-            cout << "(integer) " << r->integer << endl;
-        else if(r->type == REDIS_REPLY_ARRAY) {//返回值为数组
-            for(size_t i = 0;i < r->elements;++i)          
-                cout << i+1 << ") " << (r->elements)[i]->str << endl;
+    
+    int size = 0;
+    int l = 0;
+    char *buf;
+
+    while((l = recv(fd,&size,sizeof(size),0)) == sizeof(size)) { //接收字符串大小
         
+        cout << "size=" << size << endl;
+
+        buf = new char[size+1];
+        recv(fd,buf,size,0);    //接收命令字符串
+        buf[size] = '\0';
+
+        r = (redisReply*)redisCommand(c, buf);
+        cout << r->type << endl;
+
+        if(r->type == REDIS_REPLY_ERROR)    //发生错误
+            Send(fd,r->str,strlen(r->str));
+
+        if(r->type == REDIS_REPLY_STRING)   //返回值为字符串
+            Send(fd,r->str,strlen(r->str));
+        else if(r->type == REDIS_REPLY_NIL) {    //返回结果为空
+            command = "(nil)";
+            Send(fd,command.c_str(),command.length());
+
+        }
+        else if(r->type == REDIS_REPLY_STATUS) {    //返回命令的执行状态
+            Send(fd,r->str,strlen(r->str));
+
+        }
+        else if(r->type == REDIS_REPLY_INTEGER) {//返回值为整数 
+            command = "(integer) "+to_string(r->integer);
+            Send(fd,command.c_str(),command.length());
         }
 
-        if(!command.compare("quit"))
-            break;
-        cout << "127.0.0.1:6379 > ";
+        else if(r->type == REDIS_REPLY_ARRAY) {//返回值为数组
+            for(size_t i = 0;i < r->elements;++i) {
+                command += to_string(i+1)+") "+(r->element)[i]->str+"\n";
+            }
+            Send(fd,command.c_str(),command.length());
+        }
+        l = 0;
+        delete(buf);
+        command = "";
     }
-    return 0;
+
+}
+
+int main()
+{
+   int fd;
+    fd = socket(AF_INET,SOCK_STREAM,0);
+    if(fd < 0) {
+        cout << "套接字申请失败" << endl;
+        exit(0);
+    }
+
+    struct sockaddr_in serv;
+    memset(&serv,0,sizeof(serv));
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(5678);
+    serv.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int num = 1;
+    int length = sizeof(num);
+    setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&num,length);
+
+    if(bind(fd,(struct sockaddr *)&serv,sizeof(serv)) < 0) {
+        cout << "绑定失败 " << endl;
+        exit(0);
+    }
+
+
+    if(listen(fd,5) < 0) {
+        cout << "创建监听套接字失败" << endl;
+        exit(0);
+    }
+
+    int sockfd;
+    while((sockfd = accept(fd,NULL,NULL)) > 0) {
+        thread t(handle,sockfd);
+        t.detach();
+    }
+
+     return 0;
 }
 
